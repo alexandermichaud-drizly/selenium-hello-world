a6dc06ecfcdc7d9c91e3ebdf477f824f75b2ea9d
ARM <alexander.michaud@aya.yale.edu>
2021-02-17 22:54:16 -0500

    Got it to take a screenshot - just need to adjust screencapture settings

--- youtube_screen_record.py
+++ youtube_screen_record.py
@@ -1,13 +1,25 @@
+import sys
 from selenium import webdriver
 from time import sleep
+from subprocess import Popen
+from subprocess import call
+
+cmd = 'screencapture out'
+
+def terminate(process):
+    if process.poll() is None:
+        call('taskkill /F /T /PID ' + str(process.pid))
+
 test_url = "https://youtu.be/5Yu9UBKCwOY"
-import sys
 
 driver = webdriver.Chrome()
 driver.get(test_url)
 video = driver.find_element_by_id("player-container-outer")
+
 s = 0
+videoRecording = Popen(cmd, shell=True) # start recording
 video.click()
+
 while not driver.execute_script('return document.getElementsByTagName("video")[0].ended'):
     sys.stdout.write(f'Recording time: {s} secs')
     sys.stdout.flush()
@@ -16,4 +28,6 @@ while not driver.execute_script('return document.getElementsByTagName("video")[0
         sys.stdout.write('\b')
     s += 1
 
+terminate(videoRecording)
 driver.close()
+
